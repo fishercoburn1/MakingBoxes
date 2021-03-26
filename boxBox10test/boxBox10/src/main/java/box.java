e0120732e5682560812217f3c38623b1f6e14bea
Mitchell Melrose <43947129+Add1cted@users.noreply.github.com>
2021-03-26 09:38:00 -0500

    Added tests for boxBox10

---
+++ boxBox10test/boxBox10/src/main/java/box.java
@@ -0,0 +1,113 @@
+/**
+ * Team 2: Fisher Coburn, Matthew Pietrucha, Mitchell Melrose
+ * Professor: Brent Reeves
+ * Project: Group Project Week 8
+ *
+ * Purpose: Creates an SVG file and prints out to it
+ *
+ *how to compile/build: mvn compile
+ *						          mvn test
+ *                      java -cp target/classes box
+ */
+
+import java.io.File;  // Import the File class
+import java.io.FileWriter;
+import java.io.IOException;  // Import the IOException class to handle errors
+import java.util.Scanner;
+
+public class box
+{
+  public static String guh = "";
+  public static double w = 0;
+  public static double l = 0;
+  public static double h = 0;
+    public static void main( String[] args )  //have no idea why length seems shorter than the others
+    {
+            Scanner input = new Scanner(System.in);
+            System.out.print("What do you want the width to be in inches? ");
+            double width = input.nextInt();
+            System.out.print("What do you want the length to be in inches? ");
+            double length = input.nextInt();
+            System.out.print("What do you want the height to be in inches? ");
+            double height = input.nextInt();
+            System.out.println("Please Work");
+            work();
+  }
+
+  public static void work(double width, double length, double height)
+  {
+    try {
+      File myObj = new File("tester.svg");
+      if (myObj.createNewFile()) {
+        System.out.println("File created: " + myObj.getName());
+      } else {
+        System.out.println("File already exists.");
+      }
+    } catch (IOException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
+      try {
+
+        double addPos = width + 2.0;
+        double point = 3.0;
+
+        FileWriter myWriter = new FileWriter("tester.svg");
+        //This string "top" will just hold the declaration top part of the SVG file
+        String top = "<?xml version='1.0' encoding='us-ascii'?>\n" +
+        "<svg height=\"81.90mm\" viewBox=\"0.0 0.0 120.10 81.90\" width=\"120.10mm\"\n" +
+        "xmlns=\"http://www.w3.org/2000/svg\" xmlns:cc=\"http://creativecommons.org/ns#\"\n" +
+        "xmlns:dc=\"http://purl.org/dc/elements/1.1/\"\n" +
+        "xmlns:inkscape=\"http://www.inkscape.org/namespaces/inkscape\"\n" +
+        "xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\"\n" +
+        "xmlns:svg=\"http://www.w3.org/2000/svg/\"\n" +
+        "xmlns:xlink=\"http://www.w3.org/1999/xlink/\">\n";
+        // //The end string is just the closing out of the svg file
+        String end = "</svg>";
+
+        //The build string will handle the box pieces code - - Until we get user input these will just be the string that we are hard coding outselves to make the box
+        String[] pieces = new String[100];
+        pieces[0] = ("<g id=\"rectangle-almost\" style=\"fill:none;stroke-linecap:round;stroke-linejoin:round;\">\n" +
+                           "<path d=\"M 1.0 " + point + " H " + length * 1.5 + " v " + width + " H 1.0 V " + point + "\" stroke=\"rgb(0,0,0)\" stroke-width=\"0.20\" />\n" +
+                           "</g>\n");
+        point += addPos;
+        pieces[1] = ("<g id=\"rectangle-almost\" style=\"fill:none;stroke-linecap:round;stroke-linejoin:round;\">\n" +
+                           "<path d=\"M 1.0 " + point + " H " + length * 1.5 + " v " + width + " H 1.0 V " + point + "\" stroke=\"rgb(0,0,0)\" stroke-width=\"0.20\" />\n" +
+                           "</g>\n");
+
+        point += addPos;
+        addPos = height + 2.0;
+        pieces[2] = ("<g id=\"rectangle-almost\" style=\"fill:none;stroke-linecap:round;stroke-linejoin:round;\">\n" +
+                           "<path d=\"M 1.0 " + point + " H " + length * 1.5 + " v " + height + " H 1.0 V " + point + "\" stroke=\"rgb(0,0,0)\" stroke-width=\"0.20\" />\n" +
+                           "</g>\n");
+        point += addPos;
+        pieces[3] = ("<g id=\"rectangle-almost\" style=\"fill:none;stroke-linecap:round;stroke-linejoin:round;\">\n" +
+                           "<path d=\"M 1.0 " + point + " H " + length * 1.5 + " v " + height + " H 1.0 V " + point + "\" stroke=\"rgb(0,0,0)\" stroke-width=\"0.20\" />\n" +
+                           "</g>\n");
+        point += addPos;
+        pieces[4] = ("<g id=\"rectangle-almost\" style=\"fill:none;stroke-linecap:round;stroke-linejoin:round;\">\n" +
+                           "<path d=\"M 1.0 " + point + " H " + width * 1.5 + " v " + height + " H 1.0 V " + point + "\" stroke=\"rgb(0,0,0)\" stroke-width=\"0.20\" />\n" +
+                           "</g>\n");
+        point += addPos;
+        pieces[5] = ("<g id=\"rectangle-almost\" style=\"fill:none;stroke-linecap:round;stroke-linejoin:round;\">\n" +
+                           "<path d=\"M 1.0 " + point + " H " + width * 1.5 + " v " + height + " H 1.0 V " + point + "\" stroke=\"rgb(0,0,0)\" stroke-width=\"0.20\" />\n" +
+                           "</g>\n");
+        String finalO = (top);
+        for (int i = 0; pieces[i] != null; i++){
+          finalO += pieces[i];
+        }
+        finalO += end;
+        guh = finalO;
+        w = width;
+        l = length;
+        h = height;
+        myWriter.write(finalO);
+        // myWriter.write(top+end);
+        myWriter.close();
+        System.out.println("Successfully wrote to the file.");
+      } catch (IOException e) {
+        System.out.println("An error occurred.");
+        e.printStackTrace();
+      }
+    }
+}
---
+++ boxBox10test/boxBox10/src/test/java/boxTest.java
@@ -0,0 +1,274 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import org.junit.*;
+import java.util.Scanner;
+
+/**
+ * Unit test for svg test
+ */
+
+public class boxTest
+{
+    box aBox;
+
+    @Before
+    public void initialize() {
+        aBox = new box();
+        Scanner scan = new Scanner(System.in);
+    }
+
+    @Test
+    public void testWork()
+    {
+        try {
+            aBox.work(1,2,3);
+        } catch (Exception e) {
+            System.out.println("test catch");
+        }
+    }
+
+    /*//see if strings are equal
+    @Test
+    public void testUnit1()
+    {
+        try {
+            aBox.work(2,6,2);
+            String right = "<?xml version='1.0' encoding='us-ascii'?>\n" +
+        "<svg height=\"81.90mm\" viewBox=\"0.0 0.0 120.10 81.90\" width=\"120.10mm\"\n" + "xmlns=\"http://www.w3.org/2000/svg\" xmlns:cc=\"http://creativecommons.org/ns#\"\n" + "xmlns:dc=\"http://purl.org/dc/elements/1.1/\"\n" + "xmlns:inkscape=\"http://www.inkscape.org/namespaces/inkscape\"\n" + "xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\"\n" + "xmlns:svg=\"http://www.w3.org/2000/svg/\"\n" + "xmlns:xlink=\"http://www.w3.org/1999/xlink/\">\n" +
+
+                           "<g id=\"rectangle-almost\" style=\"fill:none;stroke-linecap:round;stroke-linejoin:round;\">\n" +
+                           "<path d=\"M 1.0 3.0 H 6.0 v 2.0 H 1.0 V 3.0\" stroke=\"rgb(0,0,0)\" stroke-width=\"0.20\" />\n" +
+                           "</g>\n" +
+                           "<g id=\"rectangle-almost1\" style=\"fill:none;stroke-linecap:round;stroke-linejoin:round;\">\n" +
+                           "<path d=\"M 1.0 7.0 H 6.0 v 2.0 H 1.0 V 7.0\" stroke=\"rgb(0,0,0)\" stroke-width=\"0.20\" />\n" +
+                           "</g>\n" +
+                           "<g id=\"rectangle-almost2\" style=\"fill:none;stroke-linecap:round;stroke-linejoin:round;\">\n" +
+                           " <path d=\"M 1.0 11.0 H 6.0 v 2.0 H 1.0 V 11.0\" stroke=\"rgb(0,0,0)\" stroke-width=\"0.20\" />\n" +
+                           "</g>\n" +
+                           "<g id=\"rectangle-almost3\" style=\"fill:none;stroke-linecap:round;stroke-linejoin:round;\">\n" +
+                           " <path d=\"M 1.0 15.0 H 6.0 v 2.0 H 1.0 V 15.0\" stroke=\"rgb(0,0,0)\" stroke-width=\"0.20\" />\n" +
+                           "</g>\n" +
+                           "<g id=\"sq-almost1\" style=\"fill:none;stroke-linecap:round;stroke-linejoin:round;\">\n" +
+                           " <path d=\"M 1.0 19.0 H 2.0 v 2.0 H 1.0 V 19.0\" stroke=\"rgb(0,0,0)\" stroke-width=\"0.20\" />\n" +
+                           "</g>\n" +
+                           "<g id=\"sq-almost2\" style=\"fill:none;stroke-linecap:round;stroke-linejoin:round;\">\n" +
+                           " <path d=\"M 1.0 23.0 H 2.0 v 2.0 H 1.0 V 23.0\" stroke=\"rgb(0,0,0)\" stroke-width=\"0.20\" />\n" +
+                           "</g>\n" +
+                           "</svg>";
+            assertEquals(aBox.guh, right );
+        } catch (Exception e) {
+            System.out.println("test catch");
+        }
+    }*/
+
+    //see if the indexes are equal to each other
+    @Test
+    public void testUnit2()
+    {
+        try {
+            aBox.work(1,1,1);
+            String right = "<?xml version='1.0' encoding='us-ascii'?>\n" +
+        "<svg height=\"81.90mm\" viewBox=\"0.0 0.0 120.10 81.90\" width=\"120.10mm\"\n" + "xmlns=\"http://www.w3.org/2000/svg\" xmlns:cc=\"http://creativecommons.org/ns#\"\n" + "xmlns:dc=\"http://purl.org/dc/elements/1.1/\"\n" + "xmlns:inkscape=\"http://www.inkscape.org/namespaces/inkscape\"\n" + "xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\"\n" + "xmlns:svg=\"http://www.w3.org/2000/svg/\"\n" + "xmlns:xlink=\"http://www.w3.org/1999/xlink/\">\n" +
+
+                           "<g id=\"line-01\" style=\"fill:none;stroke-linecap:round;stroke-linejoin:round;\">\n" +
+                           "<path d=\"M 1.0 2.0 H 11.0\" stroke=\"rgb(0,0,0)\" stroke-width=\"0.20\" />\n" +
+                           "</g>\n" +
+                           "<g id=\"corner\" style=\"fill:none;stroke-linecap:round;stroke-linejoin:round;\">\n" +
+                           "<path d=\"M 1.0 3.0 H 9.0 V 5.1 \" stroke=\"rgb(0,0,0)\" stroke-width=\"0.20\" />\n" +
+                           "</g>\n" +
+                           "<g id=\"line-u\" style=\"fill:none;stroke-linecap:round;stroke-linejoin:round;\">\n" +
+                           " <path d=\"M 1.0 4.0 H 7.0 V 8.0 H 1.0 \" stroke=\"rgb(0,0,0)\" stroke-width=\"0.20\" />\n" +
+                           "</g>\n" +
+                           "<g id=\"rectangle-almost\" style=\"fill:none;stroke-linecap:round;stroke-linejoin:round;\">\n" +
+                           "<path d=\"M 1.0 5.0 H 6.0 v 2.0 H 1.0 V 5.5 \" stroke=\"rgb(0,0,0)\" stroke-width=\"0.20\" />\n" +
+                           "</g>\n" +
+                           "<g id=\"dovetail\" style=\"fill:none;stroke-linecap:round;stroke-linejoin:round;\">\n" +
+                           "<path d=\"M 1.0 9.0 h 1.0 v 1.0 h 2.0 v -1 h 2.0 v 1 h 2.0 v -1 h 1 \" stroke=\"rgb(255,0,0)\" stroke-width=\"0.20\" />\n" +
+                           "</g>\n" +
+                           "</svg>";
+            int a = right.indexOf("?");
+            int b = aBox.guh.indexOf("?");
+            assertEquals(a, b);
+        } catch (Exception e) {
+            System.out.println("test catch");
+        }
+    }
+
+        //see if width is correct
+    @Test
+    public void testUnit3()
+    {
+        try {
+            aBox.work(2,4,6);
+            double a = 2;
+            double b = aBox.w;
+            assertEquals(a, b, 0.001);
+        } catch (Exception e) {
+            System.out.println("test catch");
+        }
+    }
+
+            //see if width is correct
+    @Test
+    public void testUnit4()
+    {
+        try {
+            aBox.work(10,4,6);
+            double a = 10;
+            double b = aBox.w;
+            assertEquals(a, b, 0.001);
+        } catch (Exception e) {
+            System.out.println("test catch");
+        }
+    }
+
+            //see if length is correct
+    @Test
+    public void testUnit5()
+    {
+        try {
+            aBox.work(2,4,6);
+            double a = 4;
+            double b = aBox.l;
+            assertEquals(a, b, 0.001);
+        } catch (Exception e) {
+            System.out.println("test catch");
+        }
+    }
+
+            //see if height is correct
+    @Test
+    public void testUnit6()
+    {
+        try {
+            aBox.work(2,4,6);
+            double a = 6;
+            double b = aBox.h;
+            assertEquals(a, b, 0.001);
+        } catch (Exception e) {
+            System.out.println("test catch");
+        }
+    }
+
+            //see if width is correct
+    @Test
+    public void testUnit7()
+    {
+        try {
+            aBox.work(44,4,6);
+            double a = 44;
+            double b = aBox.w;
+            assertEquals(a, b, 0.001);
+        } catch (Exception e) {
+            System.out.println("test catch");
+        }
+    }
+
+            //see if width * height is correct
+    @Test
+    public void testUnit8()
+    {
+        try {
+            aBox.work(2,4,6);
+            double a = 12;
+            double b = aBox.w * aBox.h;
+            assertEquals(a, b, 0.001);
+        } catch (Exception e) {
+            System.out.println("test catch");
+        }
+    }
+
+            //see if width is correct
+    @Test
+    public void testUnit9()
+    {
+        try {
+            aBox.work(200,4,6);
+            double a = 200;
+            double b = aBox.w;
+            assertEquals(a, b, 0.001);
+        } catch (Exception e) {
+            System.out.println("test catch");
+        }
+    }
+
+    //see if height is correct
+    @Test
+    public void testUnit10()
+    {
+        try {
+            aBox.work(2,4,60);
+            double a = 60;
+            double b = aBox.h;
+            assertEquals(a, b, 0.001);
+        } catch (Exception e) {
+            System.out.println("test catch");
+        }
+    }
+
+    //See if width * length is correct
+    @Test
+    public void testUnit11()
+    {
+      try {
+          aBox.work(10,20,60);
+          assertEquals(200, aBox.w*aBox.l, 0.001);
+      } catch (Exception e) {
+          System.out.println("test catch");
+      }
+    }
+
+    //See if length * height is correct
+    @Test
+    public void testUnit12()
+    {
+      try {
+          aBox.work(2,15,20);
+          assertEquals(300, aBox.l*aBox.h, 0.001);
+      } catch (Exception e) {
+          System.out.println("test catch");
+      }
+    }
+
+    //See if length, width, and height are all correct indididually
+    @Test
+    public void testUnit13()
+    {
+      try {
+          aBox.work(10,20,30);
+          assertEquals(10, aBox.w, 0.001);
+          assertEquals(20, aBox.l, 0.001);
+          assertEquals(30, aBox.h, 0.001);
+      } catch (Exception e) {
+          System.out.println("test catch");
+      }
+    }
+
+    //See if all of the multiplications are correct
+    @Test
+    public void testUnit14()
+    {
+      try {
+          aBox.work(3,4,5);
+          assertEquals(12, aBox.w*aBox.l, 0.001);
+          assertEquals(20, aBox.l*aBox.h, 0.001);
+          assertEquals(15, aBox.h*aBox.w, 0.001);
+      } catch (Exception e) {
+          System.out.println("test catch");
+      }
+    }
+
+    //See if all the multiplications are correct
+    @Test
+    public void testUnit15()
+    {
+      try {
+        aBox.work(10,5,20);
+        assertEquals(50, aBox.w*aBox.l, 0.001);
+        assertEquals(100, aBox.l*aBox.h, 0.001);
+        assertEquals(200, aBox.h*aBox.w, 0.001);
+      } catch (Exception e) {
+          System.out.println("test catch");
+      }
+    }
+}
